de8ee97c556517d6e297a84361c24d6455a7ea96
Ivar Kallejärv <ivarkallejarv@gmail.com>
2018-02-27 10:07:42 +0200

    Add gradient to jumbotron

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let elem = document.querySelector('.work-flow__iceberg');
-  let animation = elem.animate({
+  let iceberg = document.querySelector('.work-flow__iceberg');
+  let floating = iceberg.animate({
     transform: [
       'translate(-50%, -50%)',
       'translate(-50%, -52.5%)',
@@ -11,4 +11,19 @@ document.addEventListener('DOMContentLoaded', () => {
     duration: 8000,
     iterations: Infinity
   });
+
+  /*
+  let wave = document.querySelector('.work-flow__waves');
+  let continuous = wave.animate({
+    transform: [
+      'scale(1, 1)',
+      'scale(2, 1)',
+      'scale(1, 1)'
+    ],
+    easing: 'linear',
+  },{
+    duration: 8000,
+    iterations: Infinity
+  });
+  */
 }, false);
