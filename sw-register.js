af9c7caff29ae7a1a3153c9b2752b49adcb83c68
Ivar Kallejärv <ivarkallejarv@gmail.com>
2018-07-30 17:24:56 +0300

    SW support

--- sw-register.js
+++ sw-register.js
@@ -5,11 +5,7 @@ if (navigator.serviceWorker.controller) {
     console.log('[PWA] active service worker found, no need to register');
 } else {
     //Register the ServiceWorker
-    navigator.serviceWorker
-        .register('sw.js', {
-            scope: 'js/',
-        })
-        .then(function(reg) {
-            console.log('Service worker has been registered for scope:' + reg.scope);
-        });
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
+        console.log('Service worker has been registered for scope:' + reg.scope);
+    });
 }
