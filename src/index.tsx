f3167d3ba1f8fe73ee907efdc2e902e50e898acf
Tõnis Terasmaa <tonis.terasmaa@gmail.com>
2018-11-14 11:31:48 +0200

    Updated 'invisible'
    Updated the animation
    Removed the dot after 'Invisible'

--- src/index.tsx
+++ src/index.tsx
@@ -372,14 +372,30 @@ class App extends Component<any, any> {
 
         function animateInvisible() {
             const invisible = document.querySelector('.invisible');
-            const invisibleAnimation: any = {
-                opacity: [0.1, 1],
+            const initialDuration = 3000;
+            const continuousDuration = 5000;
+            const fadeAmount = 0.2;
+            const initialAnimation: any = {
+                opacity: [0.1, 0.8],
                 easing: 'ease-in-out',
             };
-            invisible!.animate(invisibleAnimation, {
-                duration: 10000,
+            const continuousAnimation: any = {
+                opacity: [0.8, fadeAmount, 0.8],
+                easing: 'ease-in-out',
+            };
+            invisible!.animate(initialAnimation, {
+                duration: initialDuration,
                 fill: 'forwards',
             });
+            setTimeout(() => {
+                for (let i = 0; i < 6; i++) {
+                    continuousAnimation.opacity = [0.8, fadeAmount + (i + 1) * 0.1, 0.8];
+                    invisible!.animate(continuousAnimation, {
+                        delay: continuousDuration * i,
+                        duration: continuousDuration,
+                    });
+                }
+            }, initialDuration);
         }
 
         /* Navigation */
@@ -559,7 +575,7 @@ class App extends Component<any, any> {
                                 <p>
                                     Driven to make
                                     <br /> complex systems feel <strong>elegant</strong> and{' '}
-                                    <strong className="invisible">invisible</strong>.
+                                    <strong className="invisible">invisible</strong>
                                 </p>
                             </div>
                             <div className="jumbotron__background" />
