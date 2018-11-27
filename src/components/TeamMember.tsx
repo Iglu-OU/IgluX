2dd828e19db8381c4018bb3d55d695d2f5fa2d77
Kaspar Suvi <kaspar.suvi@iglu.ee>
2018-11-27 18:57:15 +0200

    Rework team flexbox layout

--- src/components/TeamMember.tsx
+++ src/components/TeamMember.tsx
@@ -3,22 +3,20 @@ export default function({ data }) {
 
     return (
         <li className="team-member">
-            <a href={imageUrl} target="_blank" className="team-member-content-wrapper">
-                <p className="team-member__portrait mt-0">
-                    <img className="team-member__portrait-primary" src={imageSrc} alt={imageAlt} />
-                    <img className="team-member__portrait-secondary" src={imageSecSrc} alt={imageAlt} />
+            <p className="team-member__portrait mt-0">
+                <img className="team-member__portrait-primary" src={imageSrc} alt={imageAlt} />
+                <img className="team-member__portrait-secondary" src={imageSecSrc} alt={imageAlt} />
+            </p>
+            <div className="team-member__description">
+                <a className="team-member__name mt-0 h1" href={imageUrl} target="_blank">
+                    <span>{name}</span>
+                </a>
+                <p className="team-member__title mt-0">
+                    <span>{title}</span>
                 </p>
-                <div className="team-member__description">
-                    <h3 className="team-member__name mt-0 h1">
-                        <span>{name}</span>
-                    </h3>
-                    <p className="team-member__title mt-0">
-                        <span>{title}</span>
-                    </p>
-                    <p>{description}</p>
-                    <p>{descriptionExtended}</p>
-                </div>
-            </a>
+                <p>{description}</p>
+                <p>{descriptionExtended}</p>
+            </div>
         </li>
     );
 }
