de1a8045aa74f180d1a7e618dd2d4729f4f2ba9d
HarlesPilter <harles-herman.pilter@iglu.ee>
2019-04-02 09:35:56 +0300

    Portfolio header base

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import { PortfolioHeader } from './components/portfolio-header/PortfolioHeader';
 
 export class Portfolio extends React.PureComponent {
     render() {
         return (
             <section className="section" id="portfolio">
-                {/* TODO HEADER */}
+                <PortfolioHeader />
                 {/* TODO CONTENT */}
             </section>
         );
---
+++ src/components/portfolio/components/portfolio-header/PortfolioHeader.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class PortfolioHeader extends React.PureComponent {
+    render() {
+        return <div className="portfolio-header">test</div>;
+    }
+}
