e91b853bb5122d682e8bc8d893af93479e597da5
Kaspar Suvi <kaspar.suvi@iglu.ee>
2019-04-02 11:37:37 +0300

    Add Projects

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import { PortfolioContent } from './Content/PortfolioContent';
 
 export class Portfolio extends React.PureComponent {
     render() {
         return (
             <section className="section" id="portfolio">
                 {/* TODO HEADER */}
-                {/* TODO CONTENT */}
+                {<PortfolioContent />}
             </section>
         );
     }
