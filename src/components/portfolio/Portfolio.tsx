96afffad14997ce45f8b8c428a15a1a662a3f19a
HarlesPilter <harles-herman.pilter@iglu.ee>
2019-04-02 11:32:41 +0300

    Portfolio header base

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { PortfolioHeader } from './components/portfolio-header/PortfolioHeader';
 
+import './Portfolio.scss';
+
 export class Portfolio extends React.PureComponent {
     render() {
         return (
-            <section className="section" id="portfolio">
+            <section className="section portfolio" id="portfolio">
                 <PortfolioHeader />
                 {/* TODO CONTENT */}
             </section>
--- src/components/portfolio/components/portfolio-header/PortfolioHeader.tsx
+++ src/components/portfolio/components/portfolio-header/PortfolioHeader.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
 
+import { ReactComponent as PortfolioBG } from '../../../../assets/portfolio/background_wave.svg';
+import { ReactComponent as PortfolioIglu } from '../../../../assets/portfolio/iglu.svg';
+import { ReactComponent as PortfolioEskimos } from '../../../../assets/portfolio/eskimos.svg';
+
+import './PortfolioHeader.scss';
+
 export class PortfolioHeader extends React.PureComponent {
     render() {
-        return <div className="portfolio-header">test</div>;
+        return (
+            <div className="portfolio-header">
+                <PortfolioBG className="portfolio-header__background" />
+                <PortfolioIglu className="portfolio-header__iglu" />
+                <PortfolioEskimos className="portfolio-header__eskimos" />
+            </div>
+        );
     }
 }
