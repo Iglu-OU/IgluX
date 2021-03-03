fc09d200be5369d13dc7f2454706b6318d964469
ivark <ivarkallejarv@gmail.com>
2021-03-03 14:28:26 +0200

    Fix build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withPWA = require('next-pwa');
 
 module.exports = withPWA({
   target: 'experimental-serverless-trace',
+  future: { webpack5: true },
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
